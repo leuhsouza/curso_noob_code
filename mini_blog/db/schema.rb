fff170c4bc742f49703473493a66e4cc2946e26e
leuhsouza <leuhsouza@gmail.com>
2024-03-09 06:57:18 -0300

    feat: add posts view in home

--- mini_blog/db/schema.rb
+++ mini_blog/db/schema.rb
@@ -18,10 +18,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_115653) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "titles", force: :cascade do |t|
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
